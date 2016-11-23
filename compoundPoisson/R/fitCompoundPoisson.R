6b7e6fd5d3cb499e696e7db3d413d83549c61440
Sherman Ip <s.ip@warwick.ac.uk>
2016-11-23 16:55:17 +0000

    Implemented sampling distribution from the cplm package.

--- compoundPoisson/R/fitCompoundPoisson.R
+++ compoundPoisson/R/fitCompoundPoisson.R
@@ -1,25 +1,39 @@
 library("cplm");
 
-poisson_rate = 500;
-gamma_shape = 80;
-gamma_rate = 2;
-
 n = 100;
+poisson_rate = 1;
+gamma_shape = 1;
+gamma_rate = 1;
 
-Y = rpois(n, poisson_rate);
+n_repeat = 100;
 
-X = data.frame(x=rgamma(n, gamma_shape*Y, gamma_rate));
+nu_array = rep(0,n_repeat);
+alpha_array = rep(0,n_repeat);
+lambda_array = rep(0,n_repeat);
 
-fit = cpglm(x~1, link = "identity", X);
+for (i in seq_len(n_repeat)){
 
-phi = fit$phi
-p = fit$p
-mu = fit$coefficients
-names(mu) = NULL;
+  Y = rpois(n, poisson_rate);
+  
+  X = data.frame(x=rgamma(n, gamma_shape*Y, gamma_rate));
+  
+  fit = cpglm(x~1, link = "identity", X);
+  
+  phi = fit$phi
+  p = fit$p
+  mu = fit$coefficients
+  names(mu) = NULL;
+  
+  alpha = (2-p)/(p-1);
+  nu = mu^(2-p)/(phi*(2-p));
+  lambda = 1/(phi*(p-1)*mu^(p-1));
+  
+  nu_array[i] = nu;
+  alpha_array[i] = alpha;
+  lambda_array[i] = lambda;
+}
 
-alpha = (2-p)/(p-1);
-nu = mu^(2-p)/(phi*(2-p));
-lambda = 1/(phi*(p-1)*mu^(p-1));
+hist(nu_array);
+hist(alpha_array);
+hist(lambda_array);
 
-estimate = c(poisson_rate = nu, gamma_shape = alpha, gamma_rate = lambda);
-print(estimate);
