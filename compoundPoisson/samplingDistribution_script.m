09431aa237ca720ccdcba36616b7791e86959cbc
shermanip <s.ip@warwick.ac.uk>
2017-01-16 16:58:32 +0000

    Update line endings in compound Poisson.

--- compoundPoisson/samplingDistribution_script.m
+++ compoundPoisson/samplingDistribution_script.m
@@ -18,13 +18,13 @@ n = 100;
 %parameter set 2
 rng(138739981);
 ax_array = model.plotSamplingDistribution(n_repeat,n,3,400,100,n_bin);
-%saveas(ax_array{1},'reports/figures/compoundPoisson/sampling_dist_1_nu','epsc2');
-%saveas(ax_array{2},'reports/figures/compoundPoisson/sampling_dist_1_alpha','epsc2');
-%saveas(ax_array{3},'reports/figures/compoundPoisson/sampling_dist_1_lambda','epsc2');
+saveas(ax_array{1},'reports/figures/compoundPoisson/sampling_dist_1_nu','epsc2');
+saveas(ax_array{2},'reports/figures/compoundPoisson/sampling_dist_1_alpha','epsc2');
+saveas(ax_array{3},'reports/figures/compoundPoisson/sampling_dist_1_lambda','epsc2');
 
 %parameter set 3
 rng(81190838);
 ax_array = model.plotSamplingDistribution(n_repeat,n,500,80,2,n_bin);
-%saveas(ax_array{1},'reports/figures/compoundPoisson/sampling_dist_2_nu','epsc2');
-%saveas(ax_array{2},'reports/figures/compoundPoisson/sampling_dist_2_alpha','epsc2');
-%saveas(ax_array{3},'reports/figures/compoundPoisson/sampling_dist_2_lambda','epsc2');
+saveas(ax_array{1},'reports/figures/compoundPoisson/sampling_dist_2_nu','epsc2');
+saveas(ax_array{2},'reports/figures/compoundPoisson/sampling_dist_2_alpha','epsc2');
+saveas(ax_array{3},'reports/figures/compoundPoisson/sampling_dist_2_lambda','epsc2');
