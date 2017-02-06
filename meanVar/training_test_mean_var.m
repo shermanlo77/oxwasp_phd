f227830fd731cd50385f2a88da70dc8c4dcc1a24
shermanip <s.ip@warwick.ac.uk>
2017-02-06 21:10:31 +0000

    Added comments to the function training_test_mean_var.

--- meanVar/training_test_mean_var.m
+++ meanVar/training_test_mean_var.m
@@ -1,22 +1,36 @@
 function [mse_training_array, mse_test_array] = training_test_mean_var(data, variance_model_handle, n_train, parameter_array, n_repeat)
-    %training_test_mean_var Summary of this function goes here
-    %   Detailed explanation goes here
-    
-    threshold = reshape(data.getThreshold_topHalf(),[],1);
+    %TRAINING/TEST MEAN VAR Gets the training and test MSE when fitting and predicting the mean and variance relationship
+    %PARAMETERS:
+        %data: data object
+        %variance_model_handle: function handle to instantise a new variance model
+        %n_train: number of images to be used in estimating the mean and variance of the training set
+        %parameter_array: [1,2] x n_parameter matrix of parameters
+            %each row represent a different parameter (e.g. shape parameter and polynomial order)
+            %each column for different values of the parameters to investigate
+        %n_repeat: number of times to repeat the experiment, this is done by shuffling the training/test set
+    %RETURN:
+        %mse_training_array: n_repeat x n_parameter matrix of the training mse
+        %mse_test_array: n_repeat x n_parameter matrix of the test mse
 
+    %get the pixels which do not belong to the 3d printed sample
+    threshold = reshape(data.getThreshold_topHalf(),[],1); %reshape it to a vector
+
+    %get the size of parameter_array
     [parameter_dim, n_parameter] = size(parameter_array);
 
     %array to store the mse for each polynomial and each spilt
         %dim1: for each repeat
-        %dim2: for each polynomial order
+        %dim2: for each parameter value
     mse_training_array = zeros(n_repeat,n_parameter); %training mse
     mse_test_array = zeros(n_repeat,n_parameter); %test mse
 
-    %for each polynomialk order
+    %for each parameter
     for i_parameter = 1:n_parameter
         
+        %display the progress
         disp(strcat('i_parameter=',num2str(i_parameter)));
         
+        %for each dimension (of number) of parameters, feed it into the constructor of the variance model
         if parameter_dim == 1
             model = feval(variance_model_handle,parameter_array(i_parameter));
         elseif parameter_dim == 2
@@ -26,6 +40,7 @@ function [mse_training_array, mse_test_array] = training_test_mean_var(data, var
         %for n_repeat times
         for i_repeat = 1:n_repeat
             
+            %display the iteration number
             disp(strcat('iteration',num2str(i_repeat)));
 
             %get random index of the training and test data
@@ -56,6 +71,4 @@ function [mse_training_array, mse_test_array] = training_test_mean_var(data, var
 
     end
 
-
 end
-
