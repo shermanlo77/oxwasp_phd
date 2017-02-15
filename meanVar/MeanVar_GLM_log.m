82951d624513b6f657f61512f9030fde3d3bd14a
shermanip <s.ip@warwick.ac.uk>
2017-02-15 14:43:59 +0000

    Added class and scripts for log link in GLM fit on mean/var.

---
+++ meanVar/MeanVar_GLM_log.m
@@ -0,0 +1,34 @@
+classdef MeanVar_GLM_log < MeanVar_GLM
+    %MEANVAR_GLM_IDENTITY
+    %identity link, linear feature
+    
+    properties
+    end
+    
+    %METHODS
+    methods
+        
+        %CONSTRUCTOR
+        %PARAMETERS: see upser class MeanVar_GLM
+        function this = MeanVar_GLM_log(shape_parameter,polynomial_order)
+            %call superclass
+            this@MeanVar_GLM(shape_parameter,polynomial_order);
+        end
+        
+        %GET LINK FUNCTION DIFFERENTATED
+        %PARAMETERS:
+            %mu: column vector of means
+        %RETURN:
+            %g_dash: colum vector of g'(mu)
+        function g_dash = getLinkDiff(this,mu)
+            g_dash = 1./mu;
+        end
+        
+        %GET MEAN (LINK FUNCTION)
+        function mu = getMean(this,eta)
+            mu = exp(eta);
+        end
+    end
+    
+end
+
