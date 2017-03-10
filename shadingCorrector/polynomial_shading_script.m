dcb2ce64f8d5d3a5ddfc5ef5a23b27226bdafad3
shermanip <s.ip@warwick.ac.uk>
2017-03-10 18:08:57 +0000

    In progress report, added polynomial fit in shading correction section.

--- shadingCorrector/polynomial_shading_script.m
+++ shadingCorrector/polynomial_shading_script.m
@@ -16,26 +16,41 @@ block_data = BlockData_140316('../data/140316');
 %set up shading correction for the data set
 block_data.addShadingCorrector(@ShadingCorrector_polynomial,true,[2,2,2]);
 
+reference_name = {'black','white','grey'};
+
 %for each reference image
 for i_index = 1:3
     %plot polynomial surface
-    figure;
+    fig = figure;
+    fig.Position(3:4) = [560,420];
     imagesc(block_data.shading_corrector.reference_image_array(:,:,i_index));
     colormap gray;
+    colorbar;
+    axis(gca,'off');
+    saveas(gca,strcat('reports/figures/shadingCorrection/polynomial_',reference_name{i_index},'.eps'));
     %plot residual
-    figure;
+    fig = figure;
+    fig.Position(3:4) = [560,420];
     ax = imagesc_truncate(block_data.shading_corrector.getZResidualImage(i_index));
     ax.CLim = [-2,2];
     colorbar;
+    axis(gca,'off');
+    saveas(gca,strcat('reports/figures/shadingCorrection/residual_',reference_name{i_index},'.png'));
 end
 
 %plot the shading corrected sample scan image
-figure;
+fig = figure;
+fig.Position(3:4) = [560,420];
 imagesc_truncate(block_data.loadSample(1));
 colorbar;
 colormap gray;
+axis(gca,'off');
+saveas(gca,'reports/figures/shadingCorrection/polynomial_shadingCorrection.eps');
 
 %plot the gradient in the shading correction
-figure;
+fig = figure;
+fig.Position(3:4) = [560,420];
 imagesc_truncate(block_data.shading_corrector.b_array);
 colorbar;
+saveas(gca,'reports/figures/shadingCorrection/polynomial_gradient.png');
+axis(gca,'off');
