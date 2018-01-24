1c4246aeb83e4ca252e4b368f3c0124d61133553
shermanip <s.ip@warwick.ac.uk>
2018-01-24 15:15:19 +0000

    Added experiment for ZNull_mse.

---
+++ inference/ZNull/Experiment_ZNull_mse.m
@@ -0,0 +1,158 @@
+classdef Experiment_ZNull_mse < Experiment
+    
+    properties (SetAccess = protected)
+        rng;
+        n_bootstrap;
+        k_plot;
+        k_optima;
+        k_optima_bootstrap;
+        error_regress; %dim 1: for each k, dim 2: for each n
+        i_iteration;
+        n_iteration;
+    end
+    
+    methods (Access = public)
+        
+        %CONSTRUCTOR
+        function this = Experiment_ZNull_mse()
+            this@Experiment('ZNull_mse');
+        end
+        
+        function printResults(this)
+            previous = this.getPreviousResult();
+            for i_n = 1:numel(previous.n_array)
+    
+                array = this.getObjective(previous.var_array(:,:,i_n));
+                figure;
+                box_plot = Boxplots(array,true);
+                box_plot.setPosition(previous.k_array);
+                box_plot.plot();
+                hold on;
+                plot(this.k_plot,this.error_regress(:,i_n));
+                ylabel('ln mean squared error');
+                xlabel('kernel width');
+                title(strcat('n=',num2str(previous.n_array(i_n))));
+
+            end
+            
+            y = this.k_optima;
+            x = previous.n_array.^(-1/5);
+            glm_gamma = GlmGamma(1,IdentityLink());
+            glm_gamma.train(x,y);
+            glm_gamma.estimateShapeParameter();
+
+            boxplot_k_optima = Boxplots(this.k_optima_bootstrap,true);
+            boxplot_k_optima.setPosition((previous.n_array).^(-1/5));
+            figure;
+            boxplot_k_optima.plot();
+            hold on;
+            
+            x_plot = linspace(previous.n_array(1).^(-1/5),previous.n_array(end).^(-1/5),100);
+            [y_plot, up_error, down_error] = glm_gamma.predict(x_plot);
+            ax = plot(x_plot,y_plot);
+            plot(x_plot, up_error, 'Color', ax.Color,'LineStyle',':');
+            plot(x_plot, down_error, 'Color', ax.Color,'LineStyle',':');
+            xlabel('n^{-1/5}');
+            ylabel('optimal kernel width');
+            
+            [a,b] = glm_gamma.getParameter()
+            
+%             y_scale = std(y);
+%             y = y/y_scale;
+%             X = [ones(numel(previous.n_array),1),previous.n_array.^(-1/5)];
+%             x_shift = mean(X(:,2));
+%             x_scale = std(X(:,2));
+%             X(:,2) = (X(:,2)-x_shift)/x_scale;
+%             [~,~,stats] = glmfit(X,y,'gamma','link','identity','constant','off');
+%             y_scale * stats.beta(2)/x_scale
+%             sqrt(stats.covb(end))*y_scale/x_scale
+%             y_scale * (stats.beta(1) - stats.beta(2)*x_shift/x_scale)
+%             sqrt(y_scale^2*stats.covb(1)+(x_shift*y_scale/x_scale)^2*stats.covb(end) + 2*y_scale*(x_shift*y_scale/x_scale)*stats.covb(2))
+           
+        end
+        
+    end
+    
+    methods (Access = protected)
+        
+        %IMPLEMENTED: SET UP EXPERIMENT
+        function setup(this)
+            
+            %get the results from the ZNull experiment
+            previous = this.getPreviousResult();
+            %if the experiment is not completed, return error
+            if ~previous.is_complete
+                error('Experiment_ZNull not completed');
+            end
+            
+            %assign member variables
+            this.rng = RandStream('mt19937ar','Seed',uint32(3027942399));
+            this.n_bootstrap = 100;
+            this.k_plot = interp1(1:numel(previous.k_array),previous.k_array,linspace(1,numel(previous.k_array),10*numel(previous.k_array)));
+            this.k_optima = zeros(numel(previous.n_array),1);
+            this.k_optima_bootstrap = zeros(this.n_bootstrap,numel(previous.n_array));
+            this.error_regress = zeros(numel(this.k_plot),numel(previous.n_array));
+            
+            this.i_iteration = 0;
+            this.n_iteration = this.n_bootstrap * numel(previous.n_array);
+            
+        end
+        
+        function doExperiment(this)
+            
+            previous = this.getPreviousResult();
+            
+            for i_n = 1:numel(previous.n_array)
+                
+                ln_mse = this.getObjective(previous.var_array(:,:,i_n));
+                x =  repmat(previous.k_array,previous.n_repeat,1);
+                y =  reshape(ln_mse',[],1);
+                x(isnan(y)) = [];
+                y(isnan(y)) = [];
+                n = numel(x);
+
+                fitter = this.getRegression();
+                fitter.train(x,y);
+                this.error_regress(:,i_n) = fitter.predict(this.k_plot);
+                
+                this.k_optima(i_n) = this.findOptima(this.error_regress(:,i_n));
+                
+                for i_bootstrap = 1:this.n_bootstrap
+                    bootstrap_index = this.rng.randi(n,n,1);
+                    fitter = this.getRegression();
+                    fitter.train(x(bootstrap_index),y(bootstrap_index));
+                    error_bootstrap = fitter.predict(this.k_plot);
+                    this.k_optima_bootstrap(i_bootstrap, i_n) = this.findOptima(error_bootstrap);
+                    
+                    this.i_iteration = this.i_iteration + 1;
+                    this.printProgress(this.i_iteration / this.n_iteration);
+                end
+                
+            end
+            
+            
+        end
+        
+        function fitter = getRegression(this)
+            %fitter = KernelRegression(GaussianKernel(),0.1);
+            fitter = LocalLinearRegression(EpanechnikovKernel(),0.5);
+        end
+        
+        function previous = getPreviousResult(this)
+            previous = Experiment_ZNull();
+        end
+        
+        function objective = getObjective(this, var_array)
+            objective = log((var_array.^2-1).^2);
+        end
+        
+        function optima = findOptima(this,regress)
+            [~,i_k_optima] = min(regress);
+            optima = this.k_plot(i_k_optima);
+        end
+       
+        
+    end
+    
+end
+
--- inference/parzen/LocalLinearRegression.m
+++ inference/parzen/LocalLinearRegression.m
@@ -4,7 +4,7 @@
 %Pass the data x and y through the constructor
 %Regress using the method y_0 = getRegression(x_0)
 %Getting the kernel parameter can be done using the method setLambda(lambda)
-classdef LocalLinearRegression < handle
+classdef LocalLinearRegression < Regressor
     
     %MEMBER VARAIBLES
     properties (SetAccess = private)
@@ -13,6 +13,7 @@ classdef LocalLinearRegression < handle
         y_data; %column vector for dependent variables
         n; %number of data points
         lambda; %kernel parameter
+        kernel;
         
         %normalisation constant
         %x_data and y_data will store data which has mean 0 and std 1
@@ -31,7 +32,12 @@ classdef LocalLinearRegression < handle
         %PARAMETERS:
             %x: vector of independent variables
             %y: vector of dependent variables
-        function this = LocalLinearRegression(x,y)
+        function this = LocalLinearRegression(kernel, lambda)
+            this.kernel = kernel;
+            this.lambda = lambda;
+        end
+        
+        function train(this,x,y)
             
             %get rid of nan
             is_nan = isnan(x) | isnan(y);
@@ -52,9 +58,7 @@ classdef LocalLinearRegression < handle
             
             %get the number of data
             this.n = numel(x);
-            
-            %get the kernel parameter
-            this.lambda = 0.5;
+
         end
         
         %METHOD: SET LAMBDA
@@ -72,80 +76,78 @@ classdef LocalLinearRegression < handle
         %RETURN:
             %y_0: evaluation of the local linear regression at x_0
             %error: estimated error of y_0
-        function [y_0, error] = getRegression(this,x_0)
+        function [y] = predict(this,x)
             
-            %normalise x_0
-            x_0 = (x_0 - this.x_shift) / this.x_scale;
+            y = zeros(size(x));
             
-            %get kernel weights
-            w = this.getKernel(x_0);
-            %get the data with positive weights
-            x_sub = this.x_data(w>0);
-            y_sub = this.y_data(w>0);
-            w = w(w>0);
-            %get the number of data with positive weights
-            n_sub = numel(w);
+            x = (x - this.x_shift) / this.x_scale;
             
-            %normalise the weights so that the sum = n
-            w = (n_sub/sum(w)) * w;
-            
-            %get the square root of the weights
-            w_root = sqrt(w);
-            
-            %get the weighted design matrix
-            X = [ones(n_sub,1),x_sub];
-            X(:,1) = X(:,1) .* w_root;
-            X(:,2) = X(:,2) .* w_root;
-            
-            %get the weighted response vector
-            y_sub = w_root .* y_sub;
-            
-            %estimate the regression parameter using QR
-            [Q,R] = qr(X);
-            beta_estimate = R \ (Q'*y_sub);
-            
-            %use the beta estimate to do local linear regression
-            x_0 = [1;x_0];
-            y_0 = x_0' * beta_estimate;
-            %un-normalise y_0
-            y_0 = (y_0*this.y_scale) + this.y_shift;
-            
-            %if the error of y_0 is requested, work it out
-            if nargout >= 2
-                %get the weighted RSS divided by n degrees of freedom
-                var_estimate = mean( w.*((y_sub - X*beta_estimate).^2)) / (n_sub-2);
-                %get the matrix WX, that is diag(w) * X
-                WX = X;
-                WX(:,1) = X(:,1) .* w_root;
-                WX(:,2) = X(:,2) .* w_root;
-                %estimate the covariance matrix of beta
-                beta_cov = var_estimate * (X'*WX);
-                %estimate the variance of y_0
-                error = sqrt(x_0' * beta_cov * x_0);
-                %get the std of y_0
-                error = error * this.y_scale;
+            for i = 1:numel(x)
+                x_0 = x(i);
+
+                %get kernel weights
+                w = this.kernel.evaluate((x_0 - this.x_data)/this.lambda);
+                %get the data with positive weights
+                x_sub = this.x_data(w>0);
+                y_sub = this.y_data(w>0);
+                w = w(w>0);
+                %get the number of data with positive weights
+                n_sub = numel(w);
+
+                %normalise the weights so that the sum = n
+                w = (n_sub/sum(w)) * w;
+
+                %get the square root of the weights
+                w_root = sqrt(w);
+
+                %get the weighted design matrix
+                X = [ones(n_sub,1),x_sub];
+                X(:,1) = X(:,1) .* w_root;
+                X(:,2) = X(:,2) .* w_root;
+
+                %get the weighted response vector
+                y_sub = w_root .* y_sub;
+
+                %estimate the regression parameter using QR
+                [Q,R] = qr(X);
+                beta_estimate = R \ (Q'*y_sub);
+
+                %use the beta estimate to do local linear regression
+                x_0 = [1;x_0];
+                y_0 = x_0' * beta_estimate;
+                %un-normalise y_0
+                y_0 = (y_0*this.y_scale) + this.y_shift;
+                
+                y(i) = y_0;
+
+    %             %if the error of y_0 is requested, work it out
+    %             if nargout >= 2
+    %                 %get the weighted RSS divided by n degrees of freedom
+    %                 var_estimate = mean( w.*((y_sub - X*beta_estimate).^2)) / (n_sub-2);
+    %                 %get the matrix WX, that is diag(w) * X
+    %                 WX = X;
+    %                 WX(:,1) = X(:,1) .* w_root;
+    %                 WX(:,2) = X(:,2) .* w_root;
+    %                 %estimate the covariance matrix of beta
+    %                 beta_cov = var_estimate * (X'*WX);
+    %                 %estimate the variance of y_0
+    %                 error = sqrt(x_0' * beta_cov * x_0);
+    %                 %get the std of y_0
+    %                 error = error * this.y_scale;
+    %             end
+
+    %             %OLD CODE, estimating error in optima value of the minima
+    %             %this is done by estimating the variance of alpha/beta
+    %             optima_error = beta_cov(1)/(beta_estimate(2)^2) + beta_cov(end)*(beta_estimate(1)/(beta_estimate(2)^2))^2 - 2*beta_cov(2)*beta_estimate(1)/(beta_estimate(2)^3);
+    %             optima_error = sqrt(optima_error);
+    %             optima_error = optima_error * this.x_scale;
             end
-            
-%             %OLD CODE, estimating error in optima value of the minima
-%             %this is done by estimating the variance of alpha/beta
-%             optima_error = beta_cov(1)/(beta_estimate(2)^2) + beta_cov(end)*(beta_estimate(1)/(beta_estimate(2)^2))^2 - 2*beta_cov(2)*beta_estimate(1)/(beta_estimate(2)^3);
-%             optima_error = sqrt(optima_error);
-%             optima_error = optima_error * this.x_scale;
             
         end
         
-        %METHOD: GET KERNEL
-        %Return the kernel evaluated at x_0
-        %PARAMETERS:
-            %x_0: where the kernel shall be evaluated
-        %RETURN:
-            %k: kernel at x_0
-        function k = getKernel(this, x_0)
-            %Epanechnikov kernel
-            z = (this.x_data - x_0) / this.lambda;
-            k = zeros(this.n,1);
-            k(abs(z)>=1) = 0;
-            k(abs(z)<1) = 0.75 * (1-z(abs(z)<1).^2);
+        
+        function has_errorbar = hasErrorbar(this)
+            has_errorbar = false;
         end
         
     end
