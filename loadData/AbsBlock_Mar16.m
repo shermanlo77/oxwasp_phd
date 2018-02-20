a204a3008bf415c6da3e54d4a59b9bac96292b4e
shermanip <s.ip@warwick.ac.uk>
2018-02-20 18:44:32 +0000

    Removed half image method in Mar16.

--- loadData/AbsBlock_Mar16.m
+++ loadData/AbsBlock_Mar16.m
@@ -24,30 +24,6 @@ classdef AbsBlock_Mar16 < Scan
             segmentation((floor(this.height/2)+1):end,:) = false;
         end
         
-        %GET SAMPLE MEAN VARIANCE DATA (using top half of the images)
-        %PARAMETERS:
-            %index (optional): vector of indices of images requested to be
-            %used in mean and variance estimation, if not provided all
-            %images shall be considered
-        function [sample_mean,sample_var] = getSampleMeanVar_topHalf(this,index)
-
-            %if index not provided, index points to all images
-            if nargin == 1
-                index = 1:this.n_sample;
-            end
-            
-            %load the stack of images, indicated by the vector index
-            stack = this.loadImageStack(index);
-            %crop the stack, keeping the top half
-            stack = stack(1:(this.height/2),:,:);
-            %work out the sample mean and convert it to a vector
-            sample_mean = reshape(mean(stack,3),[],1);
-            %work out the sample variance and convert it to a vector
-            sample_var = reshape(var(stack,[],3),[],1);
-
-        end
-        
-        
 
     end
     
