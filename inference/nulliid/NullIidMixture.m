0879b6f97c85ca255e67049e579b9d7236ae5809
shermanip <s.ip@warwick.ac.uk>
2019-02-20 10:50:23 +0000

    Adjusted getSample in NullIidMixture

--- inference/nulliid/NullIidMixture.m
+++ inference/nulliid/NullIidMixture.m
@@ -21,7 +21,7 @@ classdef (Abstract) NullIidMixture < NullIid
     %Return mixture of Gaussian
     function z = getSample(this, n)
       isNull = this.randStream.rand(n,1) < 0.9;
-      z = isNull;
+      z = zeros(n,1);
       z(isNull) = this.randStream.randn(sum(isNull),1);
       z(~isNull) = this.randStream.randn(sum(~isNull),1)+3;
     end
