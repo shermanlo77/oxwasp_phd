a4bf894c5ee83a30cfdc71abef3849a8b261f025
shermanip <s.ip@warwick.ac.uk>
2019-02-20 10:25:23 +0000

    NullIidMeanVar has unique seed

--- inference/nulliid/NullIidMeanVar.m
+++ inference/nulliid/NullIidMeanVar.m
@@ -3,7 +3,7 @@ classdef NullIidMeanVar < NullIid
   methods (Access = protected)
     
     function setup(this)
-      this.setup@NullIid(uint32(4125556752));
+      this.setup@NullIid(uint32(4111000739));
     end
     
     function [nullMean, nullStd] = getNull(this, z)
