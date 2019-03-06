c74f9d20d42d4ceb830d70b19755d921a77e6712
shermanip <s.ip@warwick.ac.uk>
2019-03-06 21:50:33 +0000

    Modified bandwidthselection classes
    Altered sample space
    Sample uniformly n^{-1/5} space
    Bootstrap removed
    Use MATLAB spline, glm and opt

--- inference/bandwidthselection/BandwidthSelection.m
+++ inference/bandwidthselection/BandwidthSelection.m
@@ -7,20 +7,20 @@ classdef BandwidthSelection < Experiment
   %MEMBER VARIABLES
   properties (SetAccess = private)
     
-    rng; %random number generator
-    n_array; %array of n (sample size)
-    k_array; %array of kernel width
-    n_repeat; %number of times to repeat the simulation for a given k and n
+    rng = RandStream('mt19937ar', 'Seed', uint32(2055696007)); %random number generator
+    nArray = unique(flip(round(linspace(0.0631, 0.63, 300).^(-5))))'; %array of n (sample size)
+    kArray = linspace(0.09, 1.5, 30)'; %array of kernel width
+    nRepeat = 100;
     
     %array to store the mode and half width estimation using the Parzen density estimation
     %dim 1: for each repeat
     %dim 2: for each kernel width
     %dim 3: for each n or each sample size
-    mean_array;
-    var_array;
+    meanArray;
+    stdArray;
     
-    i_iteration; %number of iterations done
-    n_iteration; %number of iterations in the whole experiments
+    iIteration; %number of iterations done
+    nIteration; %number of iterations in the whole experiments
     
   end
   
@@ -36,7 +36,7 @@ classdef BandwidthSelection < Experiment
     function printResults(this)
       
       %meshgrid for n and k
-      [logn_plot,k_plot] = meshgrid(log10(this.n_array),this.k_array);
+      [nPlot, kPlot] = meshgrid(log10(this.nArray), this.kArray);
       factor_array = [0.9, 1.06, 1.144]; %array of fudge factors
       
       %for the mode estimation, then half width estimation
@@ -44,89 +44,64 @@ classdef BandwidthSelection < Experiment
         
         %get the corresponding array
         if i_array == 1
-          array = (squeeze(median(this.mean_array.^2)));
-          z_label = 'median squared error';
+          array = squeeze(median(this.meanArray.^2));
+          zLabel = 'median squared error';
         elseif i_array == 2
-          array = squeeze( median( (sqrt(this.var_array)-1).^2 ) );
-          z_label = 'median squared error';
+          array = squeeze(median((this.stdArray-1).^2));
+          zLabel = 'median squared error';
         else
-          array = squeeze(median(sqrt(this.var_array)));
-          z_label = '\sigma_0 estimate';
+          array = squeeze(median(this.stdArray));
+          zLabel = '\sigma_0 estimate';
         end
         
         %surf plot the error vs k and log n
         fig = LatexFigure.sub();
-        surf(k_plot,logn_plot,array);
+        surf(nPlot, kPlot, array, 'EdgeColor', 'none');
         %label axis
         xlabel('bandwidth');
         ylabel('log(n)');
-        zlabel(z_label);
+        zlabel(zLabel);
         hold on;
         
         %plot the path of the rule of thumb
         %logn_path is array of logn to evaluate the rule of thumb
-        logn_path = interp1((1:numel(this.n_array))',log10(this.n_array),linspace(1,numel(this.n_array),1000*numel(this.n_array))');
+        logn_path = interp1((1:numel(this.nArray))',log10(this.nArray), ...
+            linspace(1,numel(this.nArray),1000*numel(this.nArray))');
         if i_array ~= 3
           %for each fudge factor
           for i = 1:numel(factor_array)
             %for each logn_path, work out the rule of thumb k
             k_path = factor_array(i)*((10.^logn_path).^(-1/5));
             %then for each k and logn pair, interpolate to get the value of the array
-            path = interp2(logn_plot,k_plot,array,logn_path,k_path) + 0.01;
+            path = interp2(nPlot,kPlot,array,logn_path,k_path);
             %plot the error along the rule of thumb
-            plot3(k_path,logn_path,path,'LineWidth',3');
+            plot3(logn_path,k_path,path,'LineWidth',2');
           end
           ax = gca;
           ax.Children(numel(factor_array)).Color = [0,1,1];
         end
         %set the axis and view angle
-        xlim(this.k_array([1,numel(this.k_array)]));
-        ylim(log10(this.n_array([1,numel(this.n_array)])));
+        xlim(log10([this.nArray(1), this.nArray(end)]));
+        ylim(this.kArray([1,numel(this.kArray)]));
         
         %for the 3rd array
         if i_array==3
           hold on;
           %meshplot the true value of the null variance
-          ax = surf(k_plot,logn_plot,ones(size(array)));
+          ax = surf(nPlot,kPlot,ones(size(array)), 'EdgeColor', 'none');
           ax.FaceColor = [0.75,0.75,0.75];
           %ax.FaceAlpha = 0;
-          view(-33,20);
+          view(45,35.264);
         else
-          view(164,44);
+          view(135,35.264);
           %plot legend
           ax = gca;
           legend(ax.Children([3,2,1]),{'0.9','1.06','1.144'},'Location','best');
         end
         
         %save the figure;
-        saveas(fig,fullfile('reports','figures','inference',strcat(this.experiment_name,num2str(i_array),'.eps')),'epsc');
-        
-        %if this is a plot of the estimator itself, save the figure using a heatmap
-        linestyle_array = {'k--','k-.','k:'};
-        if i_array==3
-          fig = LatexFigure.sub();
-          %heatmap the array
-          imagesc(log10(this.n_array),this.k_array,array);
-          axis xy;
-          hold on;
-          %make a contour plot
-          contour(log10(this.n_array),this.k_array,array,[0,1],'k-');
-          %make a blank plot for the purpose of legend
-          plot([0,0],[0,0],'k-');
-          %for each factor
-          for i = 1:numel(factor_array)
-            %plot the rule of thumb
-            k_path = factor_array(i)*((10.^logn_path).^(-1/5));
-            plot(logn_path,k_path,linestyle_array{i},'LineWidth',1');
-          end
-          %label axis
-          ax = gca;
-          legend(ax.Children([4,3,2,1]),{'empirical truth','0.9','1.06','1.144'},'Location','northeast');
-          ylabel('bandwidth');
-          xlabel('log(n)');
-          colorbar;
-          saveas(fig,fullfile('reports','figures','inference',strcat(this.experiment_name,num2str(i_array+1),'.png')),'png');
-        end
+        saveas(fig,fullfile('reports','figures','inference', ...
+            strcat(this.experiment_name,'_error',num2str(i_array),'.eps')),'epsc');
         
       end
     end
@@ -138,69 +113,59 @@ classdef BandwidthSelection < Experiment
     
     %IMPLEMENTED: SETUP
     function setup(this)
-      %set random seed
-      this.rng = RandStream('mt19937ar','Seed',uint32(2055696007));
-      %declare arrays to store values to be investigated
-      this.n_array = round(10.^linspace(1,6,40))';
-      this.k_array = linspace(0.09,1.5,80)';
-      %number of times to repeat the experiment
-      this.n_repeat = 50;
       %declare array to store the mode and half width estimation using the Parzen density estimation
-      %dim 1: for each repeat
-      %dim 2: for each kernel width
-      %dim 3: for each n or each sample size
-      this.mean_array = zeros(this.n_repeat, numel(this.k_array), numel(this.n_array) );
-      this.var_array = zeros(this.n_repeat, numel(this.k_array), numel(this.n_array) );
+        %dim 1: for each repeat
+        %dim 2: for each kernel width
+        %dim 3: for each n or each sample size
+      this.meanArray = zeros(this.nRepeat, numel(this.kArray), numel(this.nArray) );
+      this.stdArray = zeros(this.nRepeat, numel(this.kArray), numel(this.nArray) );
       %assign other member variables
-      this.i_iteration = 0;
-      this.n_iteration = this.n_repeat * (numel(this.n_array)) * (numel(this.k_array));
+      this.iIteration = 0;
+      this.nIteration = this.nRepeat * numel(this.nArray) * numel(this.kArray);
     end
     
     %IMPLEMENTED: DO EXPERIMENT
     function doExperiment(this)
       
-      %for every n in n_array
-      for i_n = 1:numel(this.n_array)
+      %for every n in nArray
+      for iN = 1:numel(this.nArray)
         
         %get n or sample size
-        n = this.n_array(i_n);
+        n = this.nArray(iN);
         
         %for every kernel width
-        for i_k = 1:numel(this.k_array)
+        for iK = 1:numel(this.kArray)
           
           %get the kernel width
-          k = this.k_array(i_k);
+          k = this.kArray(iK);
           
-          %for n_repeat times
-          for i_repeat = 1:this.n_repeat
-            
+          for iRepeat = 1:this.nRepeat
+           
             %simulate n N(0,1)
-            X = this.rng.randn(n,1);
-            
+            x = this.rng.randn(n,1);
+
             %instantise z tester
-            z_tester = ZTester(X);
-            %set the kernel width
-            z_tester.setDensityEstimationParameter(k);
-            %get the mode and half width estimation
-            z_tester.estimateNull();
-            
+            empiricalNull = EmpiricalNull(x, 0, ...
+                this.rng.randi([intmin('int32'),intmax('int32')], 'int32'));
+            empiricalNull.setBandwidth(k);
+            empiricalNull.estimateNull();
+
             %save the mode and half width estimation
-            this.mean_array(i_repeat, i_k, i_n) = z_tester.mean_null;
-            this.var_array(i_repeat, i_k, i_n) = z_tester.var_null;
-            
+            this.meanArray(iRepeat, iK, iN) = empiricalNull.getNullMean();
+            this.stdArray(iRepeat, iK, iN) = empiricalNull.getNullStd();
+
             %update iteartion and progress bar
-            this.i_iteration = this.i_iteration + 1;
-            this.printProgress(this.i_iteration / this.n_iteration);
+            this.iIteration = this.iIteration + 1;
+            this.printProgress(this.iIteration / this.nIteration);
+            
           end
           
         end
         
       end
       
-      
     end
-    
-    
+     
   end
   
 end
