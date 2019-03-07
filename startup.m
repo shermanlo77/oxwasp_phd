1074c044066b8fefeb0c1a7024a5ac8f749b6b11
shermanip <s.ip@warwick.ac.uk>
2019-03-07 12:10:11 +0000

    Added startup.m and updated .gitignore

---
+++ startup.m
@@ -0,0 +1,6 @@
+addpath(genpath(fullfile('loadData')));
+addpath(genpath(fullfile('compoundPoisson')));
+addpath(genpath(fullfile('regressor')));
+addpath(genpath(fullfile('shadingCorrector')));
+addpath(genpath(fullfile('meanVar')));
+addpath(genpath(fullfile('inference')));
