631c093cd5e6974fe523be3c40f6dbd61a772483
shermanip <s.ip@warwick.ac.uk>
2019-03-14 16:08:50 +0000

    Writing empirical null section, null std represented by plotting empirical null distribution

--- inference/scripts/inferenceSubsample.m
+++ inference/scripts/inferenceSubsample.m
@@ -70,70 +70,63 @@ function subsampleExample(rowSubsample, colSubsample, name)
   fHat = parzen.getDensityEstimate(xPlot);
 
   %FIGURE
-  %Plot the frequency density estimate along with the empirical null mean and std
+  %Plot the frequency density estimate along with the empirical null
   fig = LatexFigure.sub(); 
   plot(xPlot, numel(zSampleVector)*fHat); %plot density estimate
   hold on;
-  %draw the mode as a vertical line
-  densityAtMode = parzen.getDensityEstimate(mu0);
-  plot([mu0,mu0],[0,numel(zSampleVector)*densityAtMode],'k--');
-  %draw the null std as a normpdf with the same curvature as the density estimate at the mode
-  %get x values to evalute the null pdf, choose 1 sigma
-  xPlotNull = xPlot((xPlot <  mu0+sigma0) & (xPlot >  mu0-sigma0));
-  %evalue the null pdf at these points, scale it so that the norm pdf touches the density estimate
-  nullPdfPlot = numel(zSampleVector) * densityAtMode * sqrt(2*pi) * sigma0 * ...
-      normpdf(xPlotNull, mu0, sigma0) ;
-  plot(xPlotNull,  nullPdfPlot, 'r-.');
-  %draw a horizontal line of length 2 sigma, y position of that line is where nullPdfPlot start or
-    %ends, get the value of the density estimate at mu +/- sigma
-  nullAtSigma1 = mean(nullPdfPlot([1,end]));
-  %plot line and arrows to represent 2 std
-  %triangle are plotted centred at the point when using scatter, move it so that the tip of the
-      %triangle touches the point to make it look like an arrow
-  offset = 0.1;
-  plot([mu0-sigma0,mu0+sigma0],nullAtSigma1*ones(1,2),'k--');
-  scatter(mu0-sigma0+offset,nullAtSigma1,'k<','filled');
-  scatter(mu0+sigma0-offset,nullAtSigma1,'k>','filled');
+  %draw the null std
+  nullPdfPlot = numel(zSampleVector) * parzen.getDensityEstimate(mu0) * sqrt(2*pi) * sigma0 * ...
+      normpdf(xPlot, mu0, sigma0) ;
+  plot(xPlot,  nullPdfPlot, 'r-.');
+  legend('density estimate', 'empirical null');
   ylabel('frequency density');
   xlabel('z stat');
   saveas(fig,fullfile('reports','figures','inference',strcat(name,'_densityEstimate.eps')),'epsc');
 
   %SAVE VALUE
   %save the empirical null mean
-  fileId = fopen(fullfile('reports','tables',strcat(name,'_nullMean.txt')),'w');
+  fileId = fopen(fullfile('reports','figures','inference',strcat(name,'_nullMean.txt')),'w');
   fprintf(fileId,'%.2f',mu0);
   fclose(fileId);
 
   %SAVE VALUE
   %save the empirical null std
-  fileId = fopen(fullfile('reports','tables',strcat(name,'_nullStd.txt')),'w');
+  fileId = fopen(fullfile('reports','figures','inference',strcat(name,'_nullStd.txt')),'w');
   fprintf(fileId,'%.2f',sigma0);
   fclose(fileId);
 
   %FIGURE
   %Plot the subsample z statistics along with the positive pixels
-  LatexFigure.sub();
+  fig = LatexFigure.sub();
   image_plot = Imagesc(zSampleImage);
   image_plot.addPositivePixels(zTester.positiveImage);
   image_plot.plot();
+  saveas(fig,fullfile('reports','figures','inference',strcat(name,'_subimagePositive.eps')),'epsc');
 
   %SAVE VALUE
   %Save the lower critical boundary for the empirical null BH procedure
-  fileId = fopen(fullfile('reports','tables',strcat(name,'_nullCritical1.txt')),'w');
+  fileId = fopen(fullfile('reports','figures','inference',strcat(name,'_nullCritical1.txt')),'w');
   fprintf(fileId,'%.2f',zCritical(1));
   fclose(fileId);
 
   %SAVE VALUE
   %Save the upper critical boundary for the empirical null BH procedure
-  fileId = fopen(fullfile('reports','tables',strcat(name,'_nullCritical2.txt')),'w');
+  fileId = fopen(fullfile('reports','figures','inference',strcat(name,'_nullCritical2.txt')),'w');
   fprintf(fileId,'%.2f',zCritical(2));
   fclose(fileId);
 
   %SAVE VALUE
   %Save the standarised critical boundary for the empirical null BH procedure
   zCritical = norminv(1-zTester.sizeCorrected/2);
-  fileId = fopen(fullfile('reports','tables',strcat(name,'_nullNormalisedCritical.txt')),'w');
+  fileId = fopen(fullfile('reports','figures','inference', ...
+      strcat(name,'_nullNormalisedCritical.txt')),'w');
   fprintf(fileId,'%.2f',zCritical);
+  fclose(fileId);
+  
+  %SAVE VALUE
+  %Save the number of positive pixels
+  fileId = fopen(fullfile('reports','figures','inference', strcat(name,'_nPositive.txt')),'w');
+  fprintf(fileId,'%u',sum(sum(zTester.positiveImage)));
   fclose(fileId);
 
   %FIGURE
@@ -141,12 +134,13 @@ function subsampleExample(rowSubsample, colSubsample, name)
   %Also plot the empirical null BH critical boundary
   fig = LatexFigure.sub();
   zTester.plotHistogram2(false);
-  saveas(fig,fullfile('reports','figures','inference',strcat(name,'_nullHistogram.txt')),'epsc');
+  saveas(fig,fullfile('reports','figures','inference',strcat(name,'_nullHistogram.eps')),'epsc');
 
   %FIGURE
   %plot the p values in order
   %also plot the BH critical boundary
   fig = LatexFigure.sub();
   zTester.plotPValues();
-  saveas(fig,fullfile('reports','figures','inference',strcat(name,'_nullPValues.txt')),'epsc');
+  fig.CurrentAxes.XTick = 10.^(0:5);
+  saveas(fig,fullfile('reports','figures','inference',strcat(name,'_nullPValues.eps')),'epsc');
 end
