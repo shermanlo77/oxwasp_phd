6c02d5ad581a90ef9f8718f49a6a86115830b676
shermanip <s.ip@warwick.ac.uk>
2019-06-25 15:39:11 +0100

    Format Boxplot.m and Boxplots.m

--- Boxplot.m
+++ Boxplot.m
@@ -6,192 +6,192 @@
 %Outlier data is defined using the quartiles and 1.5 IQR (standard)
 %Draws a point at the median
 classdef Boxplot < handle
+  
+  %PROPERTIES
+  properties (SetAccess = protected)
+    X; %data vector, dim 1: for each obversation
+    median; %scalar, median
+    quartiles; %2 vector, contains the 1st and 3rd quartile
+    whisker; %2 vector, contains the min and max of non-outlier data
+    outlierIndex; %boolean vector, true for outlier data
     
-    %PROPERTIES
-    properties (SetAccess = protected)
-        X; %data vector, dim 1: for each obversation
-        median; %scalar, median
-        quartiles; %2 vector, contains the 1st and 3rd quartile
-        whisker; %2 vector, contains the min and max of non-outlier data
-        outlier_index; %boolean vector, true for outlier data
-        
-        position = 0; %scalar, x position of the box plot
-        colour; %colour of the box plot
-        
-        wantOutlier = true;
-        wantMedian = true;
-        hasUpperOutlier;
-        hasLowerOutlier;
-        whisker_cap_size = 6; %size of the whisker cap
-        
-        outlier_mark = 'x'; %mark of outlier
-        outlier_size = 4; %size of the outlier mark
-        
-        legendAx; %what to show in the legend
+    position = 0; %scalar, x position of the box plot
+    colour; %colour of the box plot
+    
+    wantOutlier = true;
+    wantMedian = true;
+    hasUpperOutlier;
+    hasLowerOutlier;
+    whiskerCapSize = 6; %size of the whisker cap
+    
+    outlierMark = 'x'; %mark of outlier
+    outlierSize = 4; %size of the outlier mark
+    
+    legendAx; %what to show in the legend
+  end
+  
+  %METHODS
+  methods (Access = public)
+    
+    %CONSTURCTOR
+    function this = Boxplot(X)
+      %assign member variables
+      this.X = X;
+      colorOrder = get(groot,'defaultAxesColorOrder');
+      this.colour = colorOrder(1,:);
     end
     
-    %METHODS
-    methods (Access = public)
-        
-        %CONSTURCTOR
-        function this = Boxplot(X)
-            %assign member variables
-            this.X = X;
-            colorOrder = get(groot,'defaultAxesColorOrder');
-            this.colour = colorOrder(1,:);
-        end
-        
-        %METHOD: SET POSITION
-        %Set the x position of the box plot
-        %PARAMETERS:
-            %position: x position of the box plot
-        function setPosition(this,position)
-            this.position = position;
-        end
-        
-        %METHOD: SET COLOUR
-        %Set the colour of the box plot
-        %PARAMETERS:
-            %colour: colour of the box plot
-        function setColour(this,colour)
-            this.colour = colour;
-        end
-        
-        %METHOD: SET WANT OUTLIER
-        %Set if want to show outliers or not
-        function setWantOutlier(this, wantOutlier)
-          this.wantOutlier = wantOutlier;
-        end
-        
-        %METHOD: SET WANT MEDIAN
-        %Set if want to show the median
-        function setWantMedian(this, wantMedian)
-          this.wantMedian = wantMedian;
-        end
-        
-        %METHOD: SET WHISKER CAP SIZE
-        %Set the size of the whisker cap
-        %PARAMETERS:
-            %whisker_cap_size: size of the whisker cap
-        function setWhiskerCapSize(this,whisker_cap_size)
-            this.whisker_cap_size = whisker_cap_size;
-        end
-        
-        %METHOD: SET OUTLIER MARK
-        %Set the mark for outliers
-        %PARAMETERS:
-            %outlier_mark: mark for outliers
-        function setOutlierMark(this,outlier_mark)
-            this.outlier_mark = outlier_mark;
-        end
-        
-        %METHOD: SET OUTLIER SIZE
-        %Set the size for the outlier mark
-        %PARAMETERS:
-            %outlier_size: size of the outlier mark
-        function setOutlierSize(this,outlier_size)
-            this.outlier_size = outlier_size;
-        end
-        
-        %METHOD: PLOT
-        %Plot the box plot
-        function plot(this)
-            %set all the required statistics and save it in the member variables
-            this.getQuartiles();
-            this.getOutlier();
-            this.getWhisker();
-
-            %plot outliers
-            if (this.wantOutlier)
-              n_outlier = sum(this.outlier_index);
-              outlier = line(ones(1,n_outlier)*this.position, this.X(this.outlier_index)');
-              if n_outlier ~= 0
-                  outlier.LineStyle = 'none';
-                  outlier.Marker = this.outlier_mark;
-                  outlier.Color = this.colour;
-                  outlier.MarkerSize = this.outlier_size;
-              end
-            else
-              if (this.hasUpperOutlier)
-                  %draw an arrow at the end of the whiskers
-                  whisker_cap_upper = line(this.position,this.whisker(2));
-                  whisker_cap_upper.Marker = '^';
-                  whisker_cap_upper.Color = this.colour;
-                  whisker_cap_upper.MarkerFaceColor = this.colour;
-                  whisker_cap_upper.MarkerSize = this.whisker_cap_size;
-              elseif (this.hasLowerOutlier)
-                  whisker_cap_lower = line(this.position,this.whisker(1));
-                  whisker_cap_lower.Marker = 'v';
-                  whisker_cap_lower.Color = this.colour;
-                  whisker_cap_lower.MarkerFaceColor = this.colour;
-                  whisker_cap_lower.MarkerSize = this.whisker_cap_size;
-              end
-            end
-            
-            %draw the whisker
-            whisker_line = line([this.position,this.position],this.whisker);
-            whisker_line.Color = this.colour;
-            %the whisker is what to draw for the legend
-            this.legendAx = whisker_line;
-            
-            %draw the box
-            box = line([this.position,this.position],this.quartiles);
-            box.LineWidth = 4;
-            box.Color = this.colour;
-            
-            %draw the median if requested
-            if (this.wantMedian)
-            
-              %draw soild circle at median
-              median_outer = line(this.position,this.median);
-              median_outer.LineStyle = 'none';
-              median_outer.Marker = 'o';
-              median_outer.MarkerFaceColor = [1,1,1];
-              median_outer.Color = this.colour;
-
-              %draw point at median
-              median_inner = line(this.position,this.median);
-              median_inner.LineStyle = 'none';
-              median_inner.Marker = '.';
-              median_inner.Color = this.colour;
-              
-            end
-            
-        end
-        
+    %METHOD: SET POSITION
+      %Set the x position of the box plot
+    %PARAMETERS:
+      %position: x position of the box plot
+    function setPosition(this,position)
+      this.position = position;
     end
     
-    methods (Access = protected)
-        
-        %METHOD: GET QUARTILES
-        %Get the quartiles of the data and save it in the member variables
-        function getQuartiles(this)
-           q = quantile(this.X,[0.25,0.5,0.75]);
-           this.quartiles = zeros(1,2);
-           this.quartiles(1) = q(1);
-           this.median = q(2);
-           this.quartiles(2) = q(3);
-        end
-
-        %METHOD: GET WHISKER
-        %Get the whiskers, that is the min and max of non-outlier data
-        function getWhisker(this)
-            this.whisker = zeros(1,2);
-            this.whisker(1) = min(this.X(~this.outlier_index));
-            this.whisker(2) = max(this.X(~this.outlier_index));
-        end
-
-        %METHOD: GET OUTLIER
-        %Set which data are outliers or not, save the boolean in the member variable outlier_index
-        function getOutlier(this)
-            iqr = this.quartiles(2) - this.quartiles(1);
-            isUpperOutlier = (this.X > this.quartiles(2) + 1.5 * iqr);
-            isLowerOutlier = (this.X < this.quartiles(1) - 1.5 * iqr);
-            this.outlier_index = isLowerOutlier | isUpperOutlier;
-            this.hasUpperOutlier = any(isUpperOutlier);
-            this.hasLowerOutlier = any(isLowerOutlier);
-        end
-        
+    %METHOD: SET COLOUR
+      %Set the colour of the box plot
+    %PARAMETERS:
+      %colour: colour of the box plot
+    function setColour(this,colour)
+      this.colour = colour;
     end
     
+    %METHOD: SET WANT OUTLIER
+      %Set if want to show outliers or not
+    function setWantOutlier(this, wantOutlier)
+      this.wantOutlier = wantOutlier;
+    end
+    
+    %METHOD: SET WANT MEDIAN
+      %Set if want to show the median
+    function setWantMedian(this, wantMedian)
+      this.wantMedian = wantMedian;
+    end
+    
+    %METHOD: SET WHISKER CAP SIZE
+      %Set the size of the whisker cap
+    %PARAMETERS:
+      %whiskerCapSize: size of the whisker cap
+    function setWhiskerCapSize(this,whiskerCapSize)
+      this.whiskerCapSize = whiskerCapSize;
+    end
+    
+    %METHOD: SET OUTLIER MARK
+      %Set the mark for outliers
+    %PARAMETERS:
+      %outlierMark: mark for outliers
+    function setOutlierMark(this,outlierMark)
+      this.outlierMark = outlierMark;
+    end
+    
+    %METHOD: SET OUTLIER SIZE
+      %Set the size for the outlier mark
+    %PARAMETERS:
+      %outlierSize: size of the outlier mark
+    function setOutlierSize(this,outlierSize)
+      this.outlierSize = outlierSize;
+    end
+    
+    %METHOD: PLOT
+      %Plot the box plot
+    function plot(this)
+      %set all the required statistics and save it in the member variables
+      this.getQuartiles();
+      this.getOutlier();
+      this.getWhisker();
+      
+      %plot outliers
+      if (this.wantOutlier)
+        nOutlier = sum(this.outlierIndex);
+        outlier = line(ones(1,nOutlier)*this.position, this.X(this.outlierIndex)');
+        if nOutlier ~= 0
+          outlier.LineStyle = 'none';
+          outlier.Marker = this.outlierMark;
+          outlier.Color = this.colour;
+          outlier.MarkerSize = this.outlierSize;
+        end
+      else
+        if (this.hasUpperOutlier)
+          %draw an arrow at the end of the whiskers
+          whiskerCapUpper = line(this.position,this.whisker(2));
+          whiskerCapUpper.Marker = '^';
+          whiskerCapUpper.Color = this.colour;
+          whiskerCapUpper.MarkerFaceColor = this.colour;
+          whiskerCapUpper.MarkerSize = this.whiskerCapSize;
+        elseif (this.hasLowerOutlier)
+          whiskerCapLower = line(this.position,this.whisker(1));
+          whiskerCapLower.Marker = 'v';
+          whiskerCapLower.Color = this.colour;
+          whiskerCapLower.MarkerFaceColor = this.colour;
+          whiskerCapLower.MarkerSize = this.whiskerCapSize;
+        end
+      end
+      
+      %draw the whisker
+      whiskerLine = line([this.position,this.position],this.whisker);
+      whiskerLine.Color = this.colour;
+      %the whisker is what to draw for the legend
+      this.legendAx = whiskerLine;
+      
+      %draw the box
+      box = line([this.position,this.position],this.quartiles);
+      box.LineWidth = 4;
+      box.Color = this.colour;
+      
+      %draw the median if requested
+      if (this.wantMedian)
+        
+        %draw soild circle at median
+        medianOuter = line(this.position,this.median);
+        medianOuter.LineStyle = 'none';
+        medianOuter.Marker = 'o';
+        medianOuter.MarkerFaceColor = [1,1,1];
+        medianOuter.Color = this.colour;
+        
+        %draw point at median
+        medianInner = line(this.position,this.median);
+        medianInner.LineStyle = 'none';
+        medianInner.Marker = '.';
+        medianInner.Color = this.colour;
+        
+      end
+      
+    end
+    
+  end
+  
+  methods (Access = protected)
+    
+    %METHOD: GET QUARTILES
+    %Get the quartiles of the data and save it in the member variables
+    function getQuartiles(this)
+      q = quantile(this.X,[0.25,0.5,0.75]);
+      this.quartiles = zeros(1,2);
+      this.quartiles(1) = q(1);
+      this.median = q(2);
+      this.quartiles(2) = q(3);
+    end
+    
+    %METHOD: GET WHISKER
+    %Get the whiskers, that is the min and max of non-outlier data
+    function getWhisker(this)
+      this.whisker = zeros(1,2);
+      this.whisker(1) = min(this.X(~this.outlierIndex));
+      this.whisker(2) = max(this.X(~this.outlierIndex));
+    end
+    
+    %METHOD: GET OUTLIER
+    %Set which data are outliers or not, save the boolean in the member variable outlierIndex
+    function getOutlier(this)
+      iqr = this.quartiles(2) - this.quartiles(1);
+      isUpperOutlier = (this.X > this.quartiles(2) + 1.5 * iqr);
+      isLowerOutlier = (this.X < this.quartiles(1) - 1.5 * iqr);
+      this.outlierIndex = isLowerOutlier | isUpperOutlier;
+      this.hasUpperOutlier = any(isUpperOutlier);
+      this.hasLowerOutlier = any(isLowerOutlier);
+    end
+    
+  end
+  
 end
--- Boxplots.m
+++ Boxplots.m
@@ -1,149 +1,149 @@
 %CLASS: BOXPLOTS
 %Custom box plot class for plotting multiple box plots
 classdef Boxplots < handle
-
-    %MEMBER VARIABLES
-    properties (SetAccess = private)
-        boxplot_array; %cell array of boxplots
-        n_boxplot; %number of boxplots
-        wantTrend = false; %boolean, show trend line
-        trendLine;
-    end
+  
+  %MEMBER VARIABLES
+  properties (SetAccess = private)
+    boxplotArray; %cell array of boxplots
+    nBoxplot; %number of boxplots
+    wantTrend = false; %boolean, show trend line
+    trendLine; %graph object
+  end
+  
+  methods (Access = public)
     
-    methods (Access = public)
-    
-        %CONSTRUCTOR
-        %PARAMETERS:
-            %X: matrix, dim 1: for each obversation, dim 2: for each group or boxplot
-        function this = Boxplots(X, ~)
-            %get the number of boxplots
-            [~,this.n_boxplot] = size(X);
-            %create an array of box plots
-            this.boxplot_array = cell(1,this.n_boxplot);
-            %for each boxplot
-            for i_group = 1:this.n_boxplot
-                %instantise a boxplot and save it in this.boxplot_array
-                this.boxplot_array{i_group} = this.getBoxplot(X(:,i_group));
-                %set the position of the current boxplot
-                this.boxplot_array{i_group}.setPosition(i_group - 1);
-            end
-        end
-        
-        %METHOD: PLOT
-        %Plot each boxplot
-        function plot(this)
-            for i_group = 1:this.n_boxplot
-                this.boxplot_array{i_group}.plot();
-            end
-            %if want the trend line, plot it
-            if this.wantTrend
-              x = zeros(this.n_boxplot, 1);
-              y = zeros(this.n_boxplot, 1);
-              for i = 1:this.n_boxplot
-                x(i) = this.boxplot_array{i}.position;
-                y(i) = this.boxplot_array{i}.median;
-              end
-              this.trendLine = line(x,y);
-              this.trendLine.Color = this.boxplot_array{1}.colour;
-            end
-        end
-        
-        %METHOD: SET POSITION
-        %Set the position of each boxplot
-        %PARAMETERS:
-            %position: vector of positions
-        function setPosition(this, position)
-            for i_group = 1:this.n_boxplot
-                this.boxplot_array{i_group}.setPosition(position(i_group));
-            end
-        end
-        
-        %METHOD: SET WANT OUTLIER
-        function setWantOutlier(this, wantOutlier)
-          for i_group = 1:this.n_boxplot
-            this.boxplot_array{i_group}.setWantOutlier(wantOutlier);
-          end
-        end
-        
-        %METHOD: SET WANT TREND
-        function setWantTrend(this, wantTrend)
-          this.wantTrend = wantTrend;
-        end
-        
-        %METHOD: SET WANT MEDIAN
-        function setWantMedian(this, wantMedian)
-          for i_group = 1:this.n_boxplot
-            this.boxplot_array{i_group}.setWantMedian(wantMedian);
-          end
-        end
-        
-        %METHOD: SET COLOUR
-        %Set the colour of each boxplot
-        %PARAMETERS:
-            %colour: colour of the boxplot
-        function setColour(this, colour)
-            for i_group = 1:this.n_boxplot
-                this.boxplot_array{i_group}.setColour(colour);
-            end
-        end
-        
-        %METHOD: SET WHISKER CAP
-        %Set the whisker cap to be on of off
-        %PARAMETERS:
-            %want_whisker_cap: true if want whisker cap on
-        function setWhiskerCap(this,want_whisker_cap)
-            for i_group = 1:this.n_boxplot
-                this.boxplot_array{i_group}.setWhiskerCap(want_whisker_cap);
-            end
-        end
-        
-        %METHOD: SET WHISKER CAP SIZE
-        %Set the size of the whisker cap
-        %PARAMETERS:
-            %whisker_cap_size: size of the whisker cap
-        function setWhiskerCapSize(this,whisker_cap_size)
-            for i_group = 1:this.n_boxplot
-                this.boxplot_array{i_group}.setWhiskerCapSize(whisker_cap_size);
-            end
-        end
-        
-        %METHOD: SET OUTLIER MARK
-        %Set the mark for outliers
-        %PARAMETERS:
-            %outlier_mark: mark for outliers
-        function setOutlierMark(this,outlier_mark)
-            for i_group = 1:this.n_boxplot
-                this.boxplot_array{i_group}.setOutlierMark(outlier_mark);
-            end
-        end
-        
-        %METHOD: SET OUTLIER SIZE
-        %Set the size for the outlier mark
-        %PARAMETERS:
-            %outlier_size: size of the outlier mark
-        function setOutlierSize(this,outlier_size)
-            for i_group = 1:this.n_boxplot
-                this.boxplot_array{i_group}.setOutlierSize(outlier_size);
-            end
-        end
-        
-        %METHOD: GET LEGEND AXIS
-        %Return axis object for the purpose of legend plotting
-        function ax = getLegendAx(this)
-          ax = this.boxplot_array{1}.legendAx;
-        end
-
-    end
-    
-    methods (Access = protected)
-      
-      %METHOD: GET BOXPLOT
-      %Return a boxplot object given data X
-      function boxplot = getBoxplot(this, X)
-        boxplot = Boxplot(X);
+    %CONSTRUCTOR
+    %PARAMETERS:
+    %X: matrix, dim 1: for each obversation, dim 2: for each group or boxplot
+    function this = Boxplots(X, ~)
+      %get the number of boxplots
+      [~,this.nBoxplot] = size(X);
+      %create an array of box plots
+      this.boxplotArray = cell(1,this.nBoxplot);
+      %for each boxplot
+      for i = 1:this.nBoxplot
+        %instantise a boxplot and save it in this.boxplotArray
+        this.boxplotArray{i} = this.getBoxplot(X(:,i));
+        %set the position of the current boxplot
+        this.boxplotArray{i}.setPosition(i - 1);
       end
-      
     end
     
+    %METHOD: PLOT
+    %Plot each boxplot
+    function plot(this)
+      for i = 1:this.nBoxplot
+        this.boxplotArray{i}.plot();
+      end
+      %if want the trend line, plot it
+      if this.wantTrend
+        x = zeros(this.nBoxplot, 1);
+        y = zeros(this.nBoxplot, 1);
+        for i = 1:this.nBoxplot
+          x(i) = this.boxplotArray{i}.position;
+          y(i) = this.boxplotArray{i}.median;
+        end
+        this.trendLine = line(x,y);
+        this.trendLine.Color = this.boxplotArray{1}.colour;
+      end
+    end
+    
+    %METHOD: SET POSITION
+      %Set the position of each boxplot
+    %PARAMETERS:
+      %position: vector of positions
+    function setPosition(this, position)
+      for i = 1:this.nBoxplot
+        this.boxplotArray{i}.setPosition(position(i));
+      end
+    end
+    
+    %METHOD: SET WANT OUTLIER
+    function setWantOutlier(this, wantOutlier)
+      for i = 1:this.nBoxplot
+        this.boxplotArray{i}.setWantOutlier(wantOutlier);
+      end
+    end
+    
+    %METHOD: SET WANT TREND
+    function setWantTrend(this, wantTrend)
+      this.wantTrend = wantTrend;
+    end
+    
+    %METHOD: SET WANT MEDIAN
+    function setWantMedian(this, wantMedian)
+      for i_group = 1:this.nBoxplot
+        this.boxplotArray{i_group}.setWantMedian(wantMedian);
+      end
+    end
+    
+    %METHOD: SET COLOUR
+      %Set the colour of each boxplot
+    %PARAMETERS:
+      %colour: colour of the boxplot
+    function setColour(this, colour)
+      for i = 1:this.nBoxplot
+        this.boxplotArray{i}.setColour(colour);
+      end
+    end
+    
+    %METHOD: SET WHISKER CAP
+      %Set the whisker cap to be on of off
+    %PARAMETERS:
+      %want_whisker_cap: true if want whisker cap on
+    function setWhiskerCap(this,want_whisker_cap)
+      for i = 1:this.nBoxplot
+        this.boxplotArray{i}.setWhiskerCap(want_whisker_cap);
+      end
+    end
+    
+    %METHOD: SET WHISKER CAP SIZE
+      %Set the size of the whisker cap
+    %PARAMETERS:
+      %whiskerCapSize: size of the whisker cap
+    function setWhiskerCapSize(this,whiskerCapSize)
+      for i = 1:this.nBoxplot
+        this.boxplotArray{i}.setWhiskerCapSize(whiskerCapSize);
+      end
+    end
+    
+    %METHOD: SET OUTLIER MARK
+      %Set the mark for outliers
+    %PARAMETERS:
+      %outlierMark: mark for outliers
+    function setOutlierMark(this,outlierMark)
+      for i = 1:this.nBoxplot
+        this.boxplotArray{i}.setOutlierMark(outlierMark);
+      end
+    end
+    
+    %METHOD: SET OUTLIER SIZE
+    %Set the size for the outlier mark
+    %PARAMETERS:
+    %outlier_size: size of the outlier mark
+    function setOutlierSize(this,outlier_size)
+      for i_group = 1:this.nBoxplot
+        this.boxplotArray{i_group}.setOutlierSize(outlier_size);
+      end
+    end
+    
+    %METHOD: GET LEGEND AXIS
+      %Return axis object for the purpose of legend plotting
+    function ax = getLegendAx(this)
+      ax = this.boxplotArray{1}.legendAx;
+    end
+    
+  end
+  
+  methods (Access = protected)
+    
+    %METHOD: GET BOXPLOT
+      %Return a boxplot object given data X
+    function boxplot = getBoxplot(this, X)
+      boxplot = Boxplot(X);
+    end
+    
+  end
+  
 end
 
