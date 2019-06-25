456e61b7f665c9cf1ffea572a5293b822913c4d9
shermanip <s.ip@warwick.ac.uk>
2019-06-25 14:04:36 +0100

    Create directories on startup

--- startup.m
+++ startup.m
@@ -1,3 +1,23 @@
+directoryList = {
+  fullfile('results')
+  fullfile('results','paper')
+  fullfile('results','debug')
+  fullfile('reports')
+  fullfile('reports','figures')
+  fullfile('reports','figures','literatureReview')
+  fullfile('reports','figures','data')
+  fullfile('reports','figures','compoundPoisson')
+  fullfile('reports','figures','meanVar')
+  fullfile('reports','figures','inference')
+};
+
+for i = 1:numel(directoryList)
+  directory = directoryList{i};
+  if (exist(directory,'dir')==0)
+    mkdir(directory);
+  end
+end
+
 addpath(genpath(fullfile('scan')));
 addpath(genpath(fullfile('shadingcorrection')));
 addpath(genpath(fullfile('compoundPoisson')));
