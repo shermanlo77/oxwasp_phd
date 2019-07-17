f6a497f7eb2c125299d62dcf6f1878304dd852f7
shermanip <s.ip@warwick.ac.uk>
2019-07-17 23:26:38 +0100

    Added varMeanExample

---
+++ varmean/varMeanExample.m
@@ -0,0 +1,76 @@
+%VARIANCE MEAN EXAMPLE
+%Plots the variance-mean frequency density heatmap
+%Plots the GLM fits
+
+clearvars;
+close all;
+
+plotExample(AbsNoFilterDeg120, {[0,0,0;0,1,0], [0,0,0;1,0,0]}, {'identity', 'reciprocal'});
+plotExample(AbsFilterDeg120, {[0,0,0;1,0,0], [0,0,0;1,0,0]}, {'identity', 'reciprocal'});
+
+%NESTED FUNCTION: PLOT EXAMPLE
+%PARAMETERS:
+  %scan: scan to get the var-mean data from
+  %termMatrixArray: cell array of term matrices
+    %element 1: x^{-1}
+    %element 2: x
+    %element 3: not used
+  %linkArray: cell array of link functions
+function plotExample(scan, termMatrixArray, linkArray)
+  
+  %get the grey values
+  greyValueArray = getGreyValue(scan);
+  %get var-mean data
+  xOriginal = mean(greyValueArray,2);
+  %use x^-1 and x features, they will be multiplied to get higher order features
+  X = xOriginal.^([-1,1]);
+  yOriginal = var(greyValueArray,[],2); %get the variance of the greyvalues
+  
+  %normalise the data
+  xCentre = mean(X,1);
+  xScale = std(X,[],1);
+  X = (X-xCentre)./xScale; %noramlise
+  yStd = std(yOriginal);
+  y = yOriginal/yStd; %noramlise
+  
+  %for each model
+  for i = 1:numel(termMatrixArray)
+    
+    %fit model
+    termMatrix = termMatrixArray{i};
+    link = linkArray{i};
+    model = fitglm(X, y, termMatrix, 'Distribution', 'gamma', 'Link', link);
+    
+    %get shape parameter
+    alpha = (scan.nSample - 1) / 2;
+    
+    %get the x-y values to plot the fit
+    xPlot = linspace(min(xOriginal), max(xOriginal), 1000)';
+    XPlot = xPlot.^([-1,1]);
+    XPlot = (XPlot-xCentre)./xScale;
+    yPlot = model.predict(XPlot) * yStd;
+    
+    %get the 1 sigma error bars
+    upError = gaminv(normcdf(1), alpha, yPlot/alpha);
+    downError = gaminv(normcdf(-1), alpha, yPlot/alpha);
+    
+    %plot the heat map
+    hist3Heatmap = Hist3Heatmap();
+    fig = LatexFigure.sub();
+    hist3Heatmap.plot(xOriginal, yOriginal);
+    hold on;
+    plot(xPlot, yPlot, 'r');
+    plot(xPlot, upError, 'r--');
+    plot(xPlot, downError, 'r--');
+    xlabel('mean grey value (ADU)');
+    ylabel('variance grey value (ADU²)');
+    
+    %save the figure
+    terms = reshape(num2str(termMatrix'),1,[]);
+    terms(terms==' ') = [];
+    saveas(fig, fullfile('reports','figures','varmean', ...
+        strcat(mfilename,'_',class(scan),'_',link,terms,'.eps')),'epsc');
+    
+  end
+  
+end
