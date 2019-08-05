b9d9961d5ed85a7a4f32b7e8bcd51918233236f0
shermanip <s.ip@warwick.ac.uk>
2019-08-05 17:28:48 +0100

    Front cover

--- frontCover.m
+++ frontCover.m
@@ -3,45 +3,27 @@ close all;
 clearvars;
 
 masterImage = zeros(2000, 2000, 3);
-dilateSize = 3;
 
 scan = AbsFilterDeg120();
 projection = scan.loadImage(1);
-projectionImagesc = Imagesc(projection);
-projectionImage = projectionImagesc.plot();
-clim = projectionImagesc.clim;
-masterImage(1001:end, 1:1000, :) = projectionImage.CData(1001:end, 1:1000, :);
 
-projection = scan.calibrationScanArray(scan.whiteIndex).loadImage(1);
-projectionImagesc = Imagesc(projection);
-projectionImagesc.setCLim(clim);
-projectionImage = projectionImagesc.plot();
-masterImage(1001:end, 1001:end, :) = projectionImage.CData(1001:end, 1001:end, :);
-
-
-[test, artist, zImage] = inferenceExample();
-zTester = ZTester(zImage);
-zTester.doTest();
-
-scan.addShadingCorrectorLinear();
-projection = scan.loadImage(1);
-projectionImagesc = Imagesc(projection);
-projectionImagesc.setDilateSize(dilateSize);
-projectionImagesc.setCLim(clim);
-projectionImagesc.addPositivePixels(zTester.positiveImage);
-projectionImage = projectionImagesc.plot();
-masterImage(1:1000, 1001:end, :) = projectionImage.CData(1:1000, 1001:end, :);
-
+fig = LatexFigure.main();
+imagesc = Imagesc(projection);
+projectionImage = imagesc.plot();
+clim = imagesc.clim;
+masterImage(:, 1:1000, :) = projectionImage.CData(:, 1:1000, :);
 
 experiment = DefectDetectSubRoiAbsFilterDeg120();
 zTester = ZTester(experiment.zFilterArray(:,:,end));
 zTester.doTest();
+logp = -log10(zTester.pImage);
 
-projectionImagesc = Imagesc(projection);
-projectionImagesc.setDilateSize(dilateSize);
-projectionImagesc.setCLim(clim);
-projectionImagesc.addPositivePixels(zTester.positiveImage);
-projectionImage = projectionImagesc.plot();
-masterImage(1:1000, 1:1000, :) = projectionImage.CData(1:1000, 1:1000, :);
+fig = LatexFigure.main();
+imagesc = Imagesc(logp);
+imagesc.setCLim([0,15]);
+projectionImage = imagesc.plot();
+masterImage(:, 1001:end, :) = projectionImage.CData(:, 1001:end, :);
 
-imwrite(masterImage,'frontCover.png');
+masterImage(isnan(masterImage)) = 0;
+
+imwrite(masterImage,fullfile('reports','figures','frontCover.jpg'));
