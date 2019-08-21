9bfd77108e686cd7ef6dbe71fe68d43b8bd53d1c
shermanip <s.ip@warwick.ac.uk>
2019-08-21 15:27:15 +0100

    Compound Poisson
    Typo in CompoundPoisson.m for the objective function
    Few formatting changes and typo in report

--- compoundpoisson/CompoundPoisson.m
+++ compoundpoisson/CompoundPoisson.m
@@ -176,6 +176,7 @@ classdef CompoundPoisson < handle
         %get the observable and latent variable
         x = this.X(i);
         y = this.Y(i);
+        yVar = this.YVar(i);
         %if x is bigger than 0
         if x > 0
           %add conditional expectation terms
@@ -183,7 +184,7 @@ classdef CompoundPoisson < handle
           T(2,i) = -gammaln(y*this.alpha);
           T(3,i) = y*this.alpha*log(x);
           T(4,i) = -x*this.beta;
-          T(7,i) = -0.5*y*this.alpha^2*psi(1,y*this.alpha);
+          T(7,i) = -0.5*yVar*this.alpha^2*psi(1,y*this.alpha);
         end
         %add poisson terms
         T(5,i) = y*log(this.lambda);
@@ -204,7 +205,7 @@ classdef CompoundPoisson < handle
         %if the observable is 0, then y is 0
         if x == 0
           y = 0;
-          var = nan;
+          yVar = nan;
           %else estimate the mean and variance
         else
           %work out the normalisation constant for expectations
@@ -212,11 +213,11 @@ classdef CompoundPoisson < handle
           %work out the expectation
           y = exp(this.lnSumW(x, 1) - normalisationConstant);
           %work out the variance
-          var = exp(this.lnSumW(x, 2) - normalisationConstant) - y^2;
+          yVar = exp(this.lnSumW(x, 2) - normalisationConstant) - y^2;
         end
         %assign the expectation and variance
         this.Y(i) = y;
-        this.YVar(i) = var;
+        this.YVar(i) = yVar;
       end
     end
     
@@ -225,7 +226,7 @@ classdef CompoundPoisson < handle
     function MStep(this)
       
       %update the poisson variable
-      YMean = mean(this.Y);
+      yMean = mean(this.Y);
       
       %get non zero variables
       X0 = this.X(this.X~=0);
@@ -233,27 +234,26 @@ classdef CompoundPoisson < handle
       var0 = this.YVar(~isnan(this.YVar));
       
       %work out the gradient
-      dAlphaLnL = sum( ...
+      dAlphaT = sum( ...
           Y0*log(this.beta)...
           -Y0.*psi(this.alpha*Y0)...
           + Y0.*log(X0)...
           - (0.5*this.alpha^2).*var0.*Y0.*psi(2,Y0*this.alpha)...
           - this.alpha*var0.*psi(1,Y0*this.alpha));
-      dBetaLnL = sum(Y0*this.alpha/this.beta - X0);
+      dBetaT = sum(Y0*this.alpha/this.beta - X0);
       
       %work out the Hessian
-      dAlphaBetaLnL = sum(Y0/this.beta);
-      dAlphaAlphaLnL = -sum(...
-          Y0.^2.*psi(1,this.alpha*Y0)...
-          + var0.*psi(1,this.alpha*Y0)...
+      dAlphaBetaT = sum(Y0/this.beta);
+      dAlphaAlphaT = -sum(...
+          (Y0.^2+var0).*psi(1,this.alpha*Y0)...
           + (2*this.alpha).*var0.*Y0.*psi(2,this.alpha*Y0)...
           + (0.5*this.alpha^2).*var0.*Y0.^2.*psi(3,Y0*this.alpha));
-      dBetaBetaLnL = -sum(Y0*this.alpha/(this.beta^2));
+      dBetaBetaT = -sum(Y0*this.alpha/(this.beta^2));
       
       %put all the variables together in vector and matrix form
       theta = [this.alpha; this.beta]; %parameter matrix
-      delLnL = [dAlphaLnL; dBetaLnL]; %gradient
-      H = [dAlphaAlphaLnL, dAlphaBetaLnL; dAlphaBetaLnL, dBetaBetaLnL]; %Hessian
+      delLnL = [dAlphaT; dBetaT]; %gradient
+      H = [dAlphaAlphaT, dAlphaBetaT; dAlphaBetaT, dBetaBetaT]; %Hessian
       
       %do a newton step
       theta = theta - H\delLnL;
@@ -264,7 +264,7 @@ classdef CompoundPoisson < handle
       end
       
       %update the parameters
-      this.setParameters(YMean, theta(1), theta(2));
+      this.setParameters(yMean, theta(1), theta(2));
       
     end
     
@@ -326,7 +326,7 @@ classdef CompoundPoisson < handle
       %declare array of compound poisson terms
       %each term is a ratio of the compound poisson term with the maximum compound poisson term
       terms = zeros(1,this.nCompoundPoissonTerm);
-      %the first term is 1
+      %the first term is 1, that is exp[ln(W_ymax)-ln(W_ymax)] = 1;
       terms(1) = 1;
       
       %declare booleans isGotYL and isGotYU
