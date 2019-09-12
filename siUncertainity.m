b874aae48840e8a7c5669d645fd71bf3af41d358
shermanlo77 <s.ip@warwick.ac.uk>
2019-09-12 16:24:37 +0100

    Corrected bug in siUncertainity
    Print error bars correctly when both the value and error have the same exponent

--- siUncertainity.m
+++ siUncertainity.m
@@ -12,40 +12,43 @@
   %Does not work with value = 0, err = 0 and nTerm = 0
   %Should be that value > err
 function quote = siUncertainity(value, err, nTerm)
-
+  
   if value < 0
     isNegative = true;
     value = -value;
   else
     isNegative = false;
   end
-
+  
   %E is the exponent of value
   E = floor(log10(abs(value)));
-
   %get the mantissa of the value
-  err = err * 10^-E;
-  %get the 'mantissa' of the error
   value = value * 10^-E;
   
-  %round the error using to nTerm significant figures
+  %get the 'mantissa' of the error
+  err = err * 10^-E;
+  
+  %round the error using nTerm significant figures
   errE = floor(log10(abs(err))); %get the exponent of the 'mantissa' of the error
   err = err * 10^(-(errE - nTerm + 1)); %put nTerm figures before the decimal place
   err = round(err); %round it
-  err = num2str(err); %convert to string
-  if ( (numel(err) - nTerm) == 1 ) %99.9 rounds to 100 for example so adjust accordingly
-    err(end) = [];
+  errString = num2str(err); %convert to string
+  if ( (numel(errString) - nTerm) == 1 ) %eg 99.9 rounds to 100 for example so adjust accordingly
+    errString(end) = [];
     errE = errE + 1;
   end
   
   %round the value according to the number of decimal places in the error
   value = value * 10^(-(errE - nTerm + 1));
   value = round(value);
-  value = num2str(value);
-  value = strcat(value(1),'.',value(2:end));
+  valueString = num2str(value);
+  %if there are more than 1 value, then the mantissa needs a decimial place
+  if (numel(valueString) > 1)
+    valueString = strcat(valueString(1),'.',valueString(2:end));
+  end
   
   %to be used with \SI[separate-uncertainty = true] in latex
-  quote = strcat(value,'(',err,')E',num2str(E));
+  quote = strcat(valueString,'(',errString,')E',num2str(E));
   if (isNegative)
     quote = strcat('-',quote);
   end
